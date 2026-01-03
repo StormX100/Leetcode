4e8042c9d3645295e411a053b15fa2f137f9beb4
StormX100 <radustorm10@gmail.com>
2026-01-03 15:27:42 +0200

    Implemented "36. Valid Sudoku" problem from Leetcode

---
+++ Leetcode/36. Valid Sudoku/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+    Each row must contain the digits 1-9 without repetition.
+    Each column must contain the digits 1-9 without repetition.
+    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+*/
+
+using _36._Valid_Sudoku;
+
+string[][] validBoard =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]];
+
+Solution solution = new Solution();
+Console.WriteLine(solution.IsValidSudoku(validBoard)); // true
+
+string[][] boardWithDuplicateNumbersInRows =
+[["5","5",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]];
+
+Console.WriteLine(solution.IsValidSudoku(boardWithDuplicateNumbersInRows)); // false
+
+string[][] boardWithDuplicateNumbersInColumns =
+[["5","3",".",".","7",".",".",".","."]
+,["5",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]];
+
+Console.WriteLine(solution.IsValidSudoku(boardWithDuplicateNumbersInColumns)); // false
+
+string[][] boardWithDuplicateNumbersInBoxes =
+[[".", ".", ".", ".", "5", ".", ".", "1", "."],
+[".", "4", ".", "3", ".", ".", ".", ".", "."],
+[".", ".", ".", ".", ".", "3", ".", ".", "1"],
+["8", ".", ".", ".", ".", ".", ".", "2", "."],
+[".", ".", "2", ".", "7", ".", ".", ".", "."],
+[".", "1", "5", ".", ".", ".", ".", ".", "."],
+[".", ".", ".", ".", ".", "2", ".", ".", "."],
+[".", "2", ".", "9", ".", ".", ".", ".", "."],
+[".", ".", "4", ".", ".", ".", ".", ".", "."]];
+
+Console.WriteLine(solution.IsValidSudoku(boardWithDuplicateNumbersInBoxes)); // false
---
+++ Leetcode/36. Valid Sudoku/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _36._Valid_Sudoku
+{
+    public class Solution
+    {
+        private const int SUDOKU_LENGHT = 9;
+        private const int SUDOKU_BOX_LENGHT = 3;
+
+        public bool IsValidSudoku(string[][] board)
+        {
+            for (int i = 0; i < SUDOKU_LENGHT; i++)
+            { 
+                HashSet<string> rowNumbers = new HashSet<string>();
+                HashSet<string> columnNumbers = new HashSet<string>();
+
+                for (int j = 0; j < SUDOKU_LENGHT; j++)
+                {
+                    if (CheckNumberDuplication(rowNumbers, board[i][j]) == false) return false;
+                    if (CheckNumberDuplication(columnNumbers, board[j][i]) == false) return false;
+                }
+            }
+
+            return CheckSudokuBoxes(board);
+        }      
+
+        private bool CheckNumberDuplication(HashSet<string> numbers, string number)
+        {
+            if (number != ".")
+            {
+                if (numbers.Contains(number)) return false;
+                else numbers.Add(number);
+            }
+
+            return true;
+        }
+
+        private bool CheckSudokuBoxes(string[][] board)
+        {
+            for (int i = 1; i < SUDOKU_LENGHT; i += SUDOKU_BOX_LENGHT)
+            {
+                for (int j = 1; j < SUDOKU_LENGHT; j += SUDOKU_BOX_LENGHT)
+                {
+                    HashSet<string> boxNumbers = new HashSet<string>();
+                    if (CheckNumberDuplication(boxNumbers, board[i - 1][j - 1]) == false) return false;
+                    if (CheckNumberDuplication(boxNumbers, board[i - 1][j]) == false) return false;
+                    if (CheckNumberDuplication(boxNumbers, board[i - 1][j + 1]) == false) return false;
+                    if (CheckNumberDuplication(boxNumbers, board[i][j - 1]) == false) return false;
+                    if (CheckNumberDuplication(boxNumbers, board[i][j]) == false) return false;
+                    if (CheckNumberDuplication(boxNumbers, board[i][j + 1]) == false) return false;
+                    if (CheckNumberDuplication(boxNumbers, board[i + 1][j - 1]) == false) return false;
+                    if (CheckNumberDuplication(boxNumbers, board[i + 1][j]) == false) return false;
+                    if (CheckNumberDuplication(boxNumbers, board[i + 1][j + 1]) == false) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
