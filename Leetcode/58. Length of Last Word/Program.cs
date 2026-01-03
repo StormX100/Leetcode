d0f001f47ff54608448150d887efaba9f6864787
StormX100 <radustorm10@gmail.com>
2026-01-03 13:40:00 +0200

    Implemented "58. Length of Last Word" problem from Leetcode

---
+++ Leetcode/58. Length of Last Word/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+ A word is a maximal consisting of non-space characters only.
+*/
+
+Solution solution = new Solution();
+Console.WriteLine(solution.GetLengthOfLastWord("luffy is still joyboy")); //6
+Console.WriteLine(solution.GetLengthOfLastWord("   fly me   to   the moon  ")); //4
+
+
+public class Solution
+{
+    public int GetLengthOfLastWord(string text)
+    {
+        return text.Trim().Split(" ").Last().Length;
+    }
+}
