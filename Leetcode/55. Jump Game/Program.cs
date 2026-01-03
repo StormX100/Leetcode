2f79bb5d95cf31ff68e79b7ed2b2f798b68dc34d
StormX100 <radustorm10@gmail.com>
2026-01-03 13:36:44 +0200

    Implemented "55. Jump Game" problem from Leetcode

---
+++ Leetcode/55. Jump Game/Program.cs
@@ -0,0 +1,22 @@
+﻿/*You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.*/
+
+Solution solution = new();
+Console.WriteLine(solution.CanJump(new int[] { 2, 3, 1, 1, 4 })); // True
+Console.WriteLine(solution.CanJump(new int[] { 2, 5, 0, 0 }));  // True
+Console.WriteLine(solution.CanJump(new int[] { 3, 2, 1, 0, 4 })); // False
+Console.WriteLine(solution.CanJump(new int[] { 2, 0, 0 })); // True
+
+class Solution
+{
+    public bool CanJump(int[] nums)
+    {
+        int reachable = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] + i > reachable) reachable = nums[i] + i;
+            if (reachable == i) return i == nums.Length - 1;
+        }
+
+        return true;
+    }
+}
